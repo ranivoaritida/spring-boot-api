4e920a6d97b84f67a7e0125db706570f7b710795
Ranivoaritida Sandy <ranivoaritidasandy@gmail.com>
2025-09-18 10:49:06 +0300

    update password

--- src/main/java/com/codesand/store/controllers/UserController.java
+++ src/main/java/com/codesand/store/controllers/UserController.java
@@ -1,5 +1,6 @@
 package com.codesand.store.controllers;
 
+import com.codesand.store.dtos.ChangePasswordRequest;
 import com.codesand.store.dtos.RegisterUserRequest;
 import com.codesand.store.dtos.UpdateUserRequest;
 import com.codesand.store.dtos.UserDto;
@@ -8,6 +9,7 @@ import com.codesand.store.mappers.UserMapper;
 import com.codesand.store.repositories.UserRepository;
 import lombok.AllArgsConstructor;
 import org.springframework.data.domain.Sort;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.util.UriComponentsBuilder;
@@ -73,6 +75,24 @@ public class UserController {
         }
 
         userRepository.delete(user);
+        return ResponseEntity.noContent().build();
+    }
+
+    @PostMapping("/{id}/change-password")
+    public ResponseEntity<Void> changePassword(@PathVariable Long id,@RequestBody ChangePasswordRequest request){
+
+        var user = userRepository.findById(id).orElse(null);
+        if(user==null){
+            return ResponseEntity.notFound().build();
+        }
+
+        if(!user.getPassword().equals(request.getOldPassword())){
+            return new ResponseEntity<>(HttpStatus.UNAUTHORIZED);
+        }
+
+        user.setPassword(request.getNewPassword());
+        userRepository.save(user);
+
         return ResponseEntity.noContent().build();
     }
 
---
+++ src/main/java/com/codesand/store/dtos/ChangePasswordRequest.java
@@ -0,0 +1,9 @@
+package com.codesand.store.dtos;
+
+import lombok.Data;
+
+@Data
+public class ChangePasswordRequest {
+    private String oldPassword;
+    private String newPassword;
+}
