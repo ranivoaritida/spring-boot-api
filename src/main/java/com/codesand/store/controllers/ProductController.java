0cb71e0f9116b1a2b12489efbb6d8751e885f3e3
Ranivoaritida Sandy <ranivoaritidasandy@gmail.com>
2025-09-19 12:19:00 +0300

    create, update product

--- src/main/java/com/codesand/store/controllers/ProductController.java
+++ src/main/java/com/codesand/store/controllers/ProductController.java
@@ -3,10 +3,12 @@ package com.codesand.store.controllers;
 import com.codesand.store.dtos.ProductDto;
 import com.codesand.store.entities.Product;
 import com.codesand.store.mappers.ProductMapper;
+import com.codesand.store.repositories.CategoryRepository;
 import com.codesand.store.repositories.ProductRepository;
 import lombok.AllArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.util.UriComponentsBuilder;
 
 import java.util.List;
 
@@ -17,6 +19,7 @@ public class ProductController {
 
     public final ProductRepository productRepository;
     public final ProductMapper productMapper;
+    public final CategoryRepository categoryRepository;
 
     @GetMapping
     public List<ProductDto> getAllProducts(@RequestParam(name = "categoryId", required = false) Byte categoryId){
@@ -40,6 +43,37 @@ public class ProductController {
         if(product==null){
             return ResponseEntity.notFound().build();
         }
+        return ResponseEntity.ok(productMapper.toDto(product));
+    }
+
+    @PostMapping
+    public ResponseEntity<ProductDto> createProduct(@RequestBody ProductDto newProduct,
+                                                    UriComponentsBuilder uriBuilder){
+        var category = categoryRepository.findById(newProduct.getCategoryId()).orElse(null);
+        if(category==null){
+            return ResponseEntity.badRequest().build();
+        }
+        var product = productMapper.toEntity(newProduct);
+        product.setCategory(category);
+        productRepository.save(product);
+        var productDto = productMapper.toDto(product);
+
+        var uri = uriBuilder.path("/product/{id}").buildAndExpand(productDto.getId()).toUri();
+
+        return ResponseEntity.created(uri).body(productDto);
+
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<ProductDto> updateProduct(@PathVariable Long id, @RequestBody ProductDto request){
+        var product = productRepository.findById(id).orElse(null);
+        if(product==null){
+            return ResponseEntity.notFound().build();
+        }
+
+        productMapper.update(request,product);
+        productRepository.save(product);
+
         return ResponseEntity.ok(productMapper.toDto(product));
     }
 }
--- src/main/java/com/codesand/store/mappers/ProductMapper.java
+++ src/main/java/com/codesand/store/mappers/ProductMapper.java
@@ -1,13 +1,20 @@
 package com.codesand.store.mappers;
 
 import com.codesand.store.dtos.ProductDto;
+import com.codesand.store.dtos.UpdateUserRequest;
 import com.codesand.store.entities.Product;
 
+import com.codesand.store.entities.User;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
+import org.mapstruct.MappingTarget;
 
 @Mapper(componentModel = "spring")
 public interface ProductMapper {
     @Mapping(source = "category.id" ,target = "categoryId")
     ProductDto toDto (Product product);
+    Product toEntity (ProductDto product);
+
+    void update(ProductDto request,@MappingTarget Product product);
+
 }
