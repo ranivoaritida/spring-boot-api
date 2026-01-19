c0d1b5eadff64b54cedd1ca5fa7995f699d36c98
Ranivoaritida Sandy <ranivoaritidasandy@gmail.com>
2026-01-19 16:27:08 +0300

    authenticationManager spring security

--- src/main/java/com/codesand/store/controllers/AuthController.java
+++ src/main/java/com/codesand/store/controllers/AuthController.java
@@ -1,16 +1,14 @@
 package com.codesand.store.controllers;
 
 import com.codesand.store.dtos.LoginRequest;
-import com.codesand.store.repositories.UserRepository;
 import jakarta.validation.Valid;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Map;
 
@@ -19,25 +17,22 @@ import java.util.Map;
 @RequestMapping("/auth")
 public class AuthController {
 
-    private final UserRepository userRepository;
-    private PasswordEncoder passwordEncoder;
+    private final AuthenticationManager authenticationManager;
 
     @PostMapping("/login")
     public ResponseEntity<?> authentication(@Valid @RequestBody LoginRequest request){
-        var user = userRepository.findByEmail(request.getEmail()).orElse(null);
-        if(user == null){
-            return  ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
-        }
-        /* * if(!user.getEmail().equalsIgnoreCase(request.getEmail())){
-            return ResponseEntity.badRequest().body(
-                    Map.of("email", "This email doesn't exist.")
-            );
-        }*/
-        if(!passwordEncoder.matches(request.getPassword(), user.getPassword())){
-            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
-        }
-
+        authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(
+                        request.getEmail(),
+                        request.getPassword()
+                )
+        );
         return ResponseEntity.ok().build();
 
+    }
+
+    @ExceptionHandler(BadCredentialsException.class)
+    public ResponseEntity<Void> handleBadCredentialsException(){
+        return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
     }
 }
